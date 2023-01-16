11a6490ad56cfccca720e402e07d77814b2fa8b1
coolhand-cal <workachieveproress@gmail.com>
2023-01-16 07:35:40 -0500

    sign Up buttons activated

--- app.js
+++ app.js
@@ -8,4 +8,32 @@ const navSlide = () => {
 
 }
 
+const popUp = () => {
+   const signUpButtons = document.querySelectorAll(".signup-btn");
+   const modal = document.querySelector(".bg-modal");
+
+   for (var i = 0; i < signUpButtons.length; i++) {
+      (function(index) {
+           signUpButtons[index].addEventListener("click", () =>{
+            document.querySelector('.bg-modal').style.display ='fixed';
+              console.log("you clicked button number " + index);
+              modal.classList.toggle('modal-active');
+              console.log(modal);
+            })
+      })(i);
+}
+}
+
+const cancel = () => {
+   const cancelBtn = document.querySelector('.close-btn');
+   const modal = document.querySelector(".bg-modal");
+
+   cancelBtn.addEventListener('click', () =>{
+      modal.classList.toggle('modal-active');
+   });
+  
+  }
+
 navSlide();
+popUp();
+cancel();
